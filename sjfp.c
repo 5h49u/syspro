4925f1c401ca58830211e6b806d11b494c7a874d
Chaitanya Naidu <dummy96@protonmail.com>
2019-12-21 10:42:55 +0530

    Shortest Job First

---
+++ sjfp.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include<string.h>
+#include<malloc.h>
+
+typedef struct n {
+    char pname[20][20];
+    int arrivalT,burstT;
+     struct n *next;
+}node;
+
+node *head,*last;
+
+
+void create(char pn[20][20],int at,int bt) {
+    node *p; 
+    p = (node*)malloc(sizeof(node));
+    strcpy(p->pname,pn);
+    p->arrivalT=at;
+    p->burstT=bt;
+    p->next=NULL;
+    if(head == NULL) {
+        head=p;
+    }
+
+    else {
+        last->next=p;
+        
+    }
+
+last = p;
+}
+
+void sort() {
+    node *p,*q;
+    int temp;
+    char tem[20];
+    for(p=head;p!=NULL;p=p->next) {
+        for(q=p->next;q!=NULL; q=q->next) {
+            if(p->arrivalT > q->arrivalT) {
+                strcpy(tem,p->pname);
+                strcpy(p->pname,q->pname);
+                strcpy(q->pname,tem);
+
+                temp = p->arrivalT;
+                p->arrivalT=q->arrivalT;
+                q->arrivalT=temp;
+
+                temp=p->burstT;
+                p->burstT=q->burstT;
+                q->burstT=temp;
+            }
+        }
+    
+    }
+    printf("\nBefore\n");
+    print();
+    printf("\n");
+    for(p=head;p!=NULL;p=p->next) {
+        for (q=head;q!=NULL;q=q->next) {
+            if(p==head) {
+              
+            }
+            else {
+                
+               // if(p->burstT > q->arrivalT) {
+                    if(p->burstT > q->burstT) {
+                        printf("\nWent here\n");
+                        print();
+                        strcpy(tem,p->pname);
+                        strcpy(p->pname,q->pname);
+                        strcpy(q->pname,tem);
+
+                        temp = p->arrivalT;
+                        p->arrivalT=q->arrivalT;
+                        q->arrivalT=temp;
+
+                        temp=p->burstT;
+                        p->burstT=q->burstT;
+                        q->burstT=temp;
+                        printf("\n");
+                        print();
+                    }
+               // }
+            }
+        }
+    }
+
+}
+
+void print() {
+    node *p;
+
+    for(p=head;p!=NULL;p=p->next) {
+        printf("%s\t",p->pname);
+    }
+}
+
+int main() {
+    int n,arrivalT,burstT;
+    char pname[20][20];
+    printf("How many process do you want to run ?");
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++) {
+        printf("Enter the process name, Arrival Time, Burst Time-:\n");
+        scanf("%s%d%d",&pname,&arrivalT,&burstT);
+        create(pname,arrivalT,burstT);
+    }
+
+    sort();
+   // calc();
+   print();
+
+
+
+
+
+}
