c146905cfc45bfed6f6749d46ca73bdd242efbce
ubuntu-8 <dummy96@protonmail.com>
2019-12-23 10:11:39 +0530

    SJPF

--- sjfp.c
+++ sjfp.c
@@ -54,8 +54,69 @@ void swap (node *p,node *q) {
 }
 
 void sort () {
+     node *p,*q;
+     int end=0;
+     for(p=head;p!=NULL;p=p->next) { //Sort according to arrival time
+          for(q=p->next;q!=NULL;q=q->next) {
+               if(p->arrivalT > q->arrivalT) {
+               swap(p,q);
+          
+          }
     
 }
+end = end + p->burstT;
+}
+      for(p=head;p!=NULL;p=p->next) {
+          for(q=p->next;q!=NULL;q=q->next) {
+               if(p->burstT > q->burstT && p!=head){
+               swap(p,q);
+               }
+               else {
+                    for(int i=p->arrivalT ;i < p->burstT ; i++) { 
+                         if(i == q->arrivalT && ((p->burstT)-i) > q->burstT) {
+                              end= end-((p->burstT)-i);
+                             create(p->pname,end,((p->burstT)-i));
+                             p->burstT=i;
+                              
+                         } 
+                    
+                    }
+                    }
+                    }
+                    }
+                    }
+void calc() //Calculate Start time ,End Time, Turn around time, Wait time for each process(Common logic for all Scheduling Programs)
+{
+    node *p, *q;
+    for (p = head; p != NULL; p = p->next)
+    {
+        if (p == head)
+        {
+            p->startT = p->arrivalT;        //Start time for each process would be its arrival time
+            p->endT = p->burstT;            //End time for first process would be its burst time
+            p->tat = p->endT - p->arrivalT; //Turn around time = Exit time - arrival time
+            p->waitT = p->tat - p->burstT;  // Wait time = Turn around time - Burst time of the process
+        }
+        for (q = p->next; q != NULL; q = q->next)
+        {
+
+                q->startT = p->endT;         //Start time of subsequent process would be the end time of previous process
+            q->endT = q->startT + q->burstT; //End time would be the Start time + Burst time of the process
+            q->tat = q->endT - q->arrivalT;
+            q->waitT = q->tat - q->burstT;
+
+            break;
+        }
+    }
+}
+          
+void print() {
+     node *p;
+     for(p=head;p!=NULL;p=p->next) {
+          printf("%s\t%d\t%d\t%d\t%d\n",p->pname,p->arrivalT,p->burstT,p->startT,p->endT);
+          }
+         }
+     
 
 int main()
 {
@@ -74,7 +135,7 @@ int main()
     sort();
     calc();
     print();
-    printf("\nAverage Waiting time:%f", (float)totalwt / n);
-    printf("\nAverage Turn Around Time:%f\n", (float)totaltat / n);
-    printg();
+    //printf("\nAverage Waiting time:%f", (float)totalwt / n);
+   // printf("\nAverage Turn Around Time:%f\n", (float)totaltat / n);
+   // printg();
 }
